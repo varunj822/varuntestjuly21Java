3718e343df31a0ffd671246f7ae3cd1625a9c02c
Varun Joshi <varunjoshi8822@gmail.com>
2021-08-31 17:34:14 +0530

    Varun_Branch_A

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("Hello TechnoCredits");
+		System.out.println("Hello TechnoCredits, How are you? ");
 	}
 }
